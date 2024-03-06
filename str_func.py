3373a276f5cd4eb37d5776a6595b5f940964e65f
Sveta Lalayan <lalayan-svetlana@list.ru>
2024-03-07 00:25:21 +0300

    the second function

--- str_func.py
+++ str_func.py
@@ -1,5 +1,12 @@
 def capitalize_all_letters():
     input()
-
+'''Делает буквые большими'''
 result = input()
 print(result.upper())
+
+
+def capitalize():
+    input()
+'''Функция делает первые буквы слов заглавными'''
+result = input()
+print(result.title())
